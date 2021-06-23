c05d2a3ac05d9c53bba21263dd44d6570bc5f465
talentdev2020 <polaris.center@outlook.com>
2021-06-22 19:40:42 -0700

    remove headercontainer

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import Product from "./pages/product"
 import { useDispatch } from 'react-redux';
 import { getProducts } from './modules/product';
-import HeaderContainer from "./container/header";
 import CartContainer from "./container/cart";
 import CartFooterContainer from "./container/cartfooter";
 import Drawer from "./components/drawer";
